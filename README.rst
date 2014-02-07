0baca697888a2e60d4da2c3c4c1df5c7f1ccdb30
Franco Saliola <saliola@gmail.com>
2014-02-07 10:35:41 -0500

    xmonad.hs and installation/config. instructions

--- README.rst
+++ README.rst
@@ -1,4 +1,135 @@
+========================
 My .xmonad configuration
 ========================
 
+------------------------------
+Installation in two easy steps
+------------------------------
+
+Clone repo::
+
+    git clone https://github.com/saliola/dotxmonad.git ~/.xmonad
+
+Recompile and restart xmonad::
+
+    xmonad --recompile
+    xmonad --restart
+
+
+-------------------------------------------------------------
+Instructions on configuring xmonad with gnome-panel on Ubuntu
+-------------------------------------------------------------
+
+These are instructions to get xmonad working with Gnome, Gnome panel, and dmenu
+(as launcher) on ubuntu-13.10.
+
+.. note::
+
+    - Xsession errors are logged to ~/.xsession-errors.
+    - You cannot start a panel or launcher from within tmux (perhaps a
+      reattached tmux session that was launched in another X session...);
+      so use a new terminal instead.
+
+.. warning::
+
+    - I had issues with the system finding the correct xmonad.desktop or
+      xmonad.session file (not sure what the problem was, however).
+      If there are problems, move the default files provided by Ubuntu out
+      of the way.
+
+- install xmonad, gnome-panel and dmenu (for use with Mod-p)::
+
+    apt-get install xmonad
+    apt-get install gnome-panel
+    apt-get install suckless-tools         # dmenu
+
+- If ``/usr/share/applications/xmonad.desktop`` does not exist, then create it;
+  the following is the file that is distributed with the xmonad package::
+
+    [Desktop Entry]
+    Type=Application
+    Name=Xmonad
+    Exec=xmonad
+    NoDisplay=true
+    X-GNOME-WMName=Xmonad
+    X-GNOME-Autostart-Phase=WindowManager
+    X-GNOME-Provides=windowmanager
+    X-GNOME-Autostart-Notify=true
+
+- create ``/usr/share/xsessions/xmonad.desktop`` (move any existing file with
+  this name out of the way)::
+
+    [Desktop Entry]
+    Name=Xmonad-Gnome Hybrid of Amazingness
+    Comment=Tiling window manager
+    TryExec=/usr/bin/gnome-session
+    Exec=gnome-session --session=xmonad
+    Type=XSession
+
+- create ``/usr/share/gnome-session/sessions/xmonad.session``::
+
+    [GNOME Session]
+    Name=Xmonad-Gnome Hybrid of Amazingness
+    RequiredComponents=gnome-settings-daemon;gnome-panel;xmonad
+
+- clone/copy/link ``~/.xmonad/xmonad.hs`` into place
+
+- launch gnome-terminal and create a profile with the following
+  settings (this is used by the "scratchpad" terminal (Mod-t))::
+
+    General
+    > Profile name: scratchpad
+    > [Deselect] Show menubar by default in new terminals
+
+    Title and Command
+    > Initial title: scratchpad
+    > When terminal commands set their own titles: Keep initial title
+
+    Scrolling
+    > Scrollbar is: Disabled
+    > Scrollback: Unlimited
+
+- prevent nautilus from handling the desktop.
+
+    Execute ``dconf-editor``, then go to::
+
+        org > gnome > desktop > background
+
+    deselect::
+
+        show-desktop-icons
+
+    Possibly, you might have to deselect::
+
+        draw-background
+
+- log out; log in and enjoy!
+
+-----
+Notes
+-----
+
+- Previously, I had xmonad configured to work with the ``unity-2d-launcher``
+  and the ``unity-2d-panel``. I used the following
+  ``/usr/share/gnome-session/sessions/xmonad.session`` file::
+
+    [GNOME Session]
+    Name=Xmonad-Gnome Hybrid of Amazingness
+    RequiredComponents=gnome-settings-daemon;
+    RequiredProviders=windowmanager;panel;launcher;
+    DefaultProvider-windowmanager=xmonad
+    DefaultProvider-panel=unity-2d-panel
+    DefaultProvider-launcher=unity-2d-launcher
+
+  However, with Ubuntu 13.04, the commands ``unity-2d-panel`` and
+  ``unity-2d-launcher`` are no longer available as Ubuntu migrated from
+  unity-2d-panel and unity-2d-launcher to the combined unity interface.
+
+- I haven't looked into this yet, but it could be cool to integrate Ubuntu's
+  HUD with XMonad.
+
+**References**
+
+- `Xmonad/Using xmonad in Gnome <http://www.haskell.org/haskellwiki/Xmonad/Using_xmonad_in_Gnome>`_
+- `Using Xmonad in Unity 2D <http://www.haskell.org/haskellwiki/Xmonad/Using_xmonad_in_Unity_2D>`_
 
---
+++ xmonad.hs
@@ -0,0 +1,146 @@
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses #-}
+
+-- after editing, running
+--
+--     xmonad --recompile
+--     xmonad --restart
+
+-- NOTES:
+-- - There is some code below from when I configured XMonad to integrate with
+-- unity-2d-panel and unity-2d-launcher. But Ubuntu migrated from these to the
+-- combined unity interface. I haven't looked into this yet, but it could be
+-- cool to integrate Ubuntu's HUD with XMonad (as a launcher, at the very
+-- least). I left the code dealing with creating a Gap for the launcher in case
+-- I decide to include another launcher.
+
+-- FEATURES:
+-- - configured to work with gnome-panel:
+--     - includes gnome-panel, but no launcher
+--     - mod+b toggles displaying the panel (default behaviour)
+-- - uses smartBorders:
+--     - no border if there is only one window
+-- - uses ResizableTall to allow for resizing windows:
+--     - mod+s and mod+x for shrink/expand
+-- - gvim keybinding:
+--     - mod+g spans gvim
+-- - maximize keybinding (Layout.Maximize):
+--     - mod+z zooms in on a window
+-- - workspace navigation (Actions.CycleWS):
+--     - mod+` switches to previous workspace
+--     - mod+ctrl+left/right switchs to previous/next workspace
+--     - mod+shift+left/right shifts focused window to previous/next workspace
+-- - BoringWindows: skip boring windows
+--     - mod+shift+B mark window as boring
+--     - mod+shift+ctrl+B clear marks
+--     - mod+j/mod+k cycle but skip boring windows
+-- - Gaps: create gap on left side for the Unity launcher
+--     - mod+u toggle Unity launcher
+--     - this is an artifact of when I configured this to work with the Unity launcher
+-- - Scratchpad: terminal
+--     - mod+t spawns terminal, or brings terminal to current workspace, or
+--       moves terminal to hidden workspace called NSP
+--     - in order for this to work, there must be a gnome-terminal profile
+--       called "scratchpad" and the title of the window must by "scratchpad";
+--       I also like to disable the scrollbars and the menubar.
+
+import XMonad
+import XMonad.Util.EZConfig
+import XMonad.Config.Gnome
+import XMonad.ManageHook
+import XMonad.Hooks.ManageDocks
+import XMonad.Layout.NoBorders
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.Maximize
+import XMonad.Actions.CycleWS
+import XMonad.Layout.BoringWindows as BoringWindows
+import XMonad.Layout.Tabbed
+import XMonad.Layout.Gaps
+import XMonad.Util.NamedScratchpad
+import XMonad.StackSet as StackSet
+
+main = xmonad $ myConfig
+
+myManageHook :: [ManageHook]
+-- EXAMPLE: how to tell XMonad to ignore a particular window
+-- myManageHook =  [ className =? "Unity-2d-panel"    --> doIgnore
+--                 , className =? "Unity-2d-launcher" --> doIgnore
+--                 ]
+myManageHook =  []
+
+scratchpads = [
+ -- run xterm, find it by title, place it in a floating window: the size is
+ -- given as (StackSet.RationalRect l t w h), where h is height, w is width,
+ -- t is distance from top, l is distance from left (percentage)
+      NS "gnome-terminal"
+         "gnome-terminal --window-with-profile=scratchpad --disable-factory --title scratchpad"
+         (title =? "scratchpad")
+         (customFloating $ StackSet.RationalRect (0) (7/10) (1) (3/10))
+ -- run antidote, find by title, don't float
+    , NS "antidote"
+         "/home/saliola/Applications/bin/antidote"
+         (title =? "Nouveau document - Antidote")
+         nonFloating
+ ]
+
+-- EXAMPLES OF SCRATCHPADS
+-- scratchpads = [
+--  -- run gvim, find by role, place it in a floating window
+--     , NS "notes"
+--          "gvim --role notes ~/notes.txt"
+--          (role =? "notes")
+--          (customFloating $ StackSet.RationalRect (1/6) (1/6) (2/3) (2/3))
+--  -- run gvim, find by role, don't float
+--      NS "notes" "gvim --role notes ~/notes.txt" (role =? "notes") nonFloating
+--  ] where role = stringProperty "WM_WINDOW_ROLE"
+
+manageNamedScratchPad :: ManageHook
+manageNamedScratchPad = namedScratchpadManageHook scratchpads
+
+myConfig = gnomeConfig
+    { modMask = myModMask
+    -- , layoutHook = gaps [(L, 66)] $ boringWindows $ avoidStruts $ smartBorders $
+    , layoutHook = boringWindows $ avoidStruts $ smartBorders $
+                (maximize tiled ||| maximize (Mirror tiled) ||| Full ||| simpleTabbed)
+    , manageHook = manageHook gnomeConfig <+> composeAll myManageHook <+> manageNamedScratchPad
+    }
+    `additionalKeys`
+        [
+        -- ResizableTile
+          ((myModMask, xK_x), sendMessage MirrorShrink)
+        , ((myModMask, xK_s), sendMessage MirrorExpand)
+        -- Maximize
+        , ((myModMask, xK_z), withFocused (sendMessage . maximizeRestore))
+        -- CycleWS
+        , ((myModMask, xK_grave), toggleWS)
+        , ((myModMask .|. controlMask, xK_Left), prevWS)
+        , ((myModMask .|. controlMask, xK_Right), nextWS)
+        , ((myModMask .|. shiftMask, xK_Left), shiftToPrev >> prevWS)
+        , ((myModMask .|. shiftMask, xK_Right), shiftToNext >> nextWS)
+        -- BoringWindows
+        , ((myModMask, xK_k), BoringWindows.focusUp)
+        , ((myModMask, xK_j), BoringWindows.focusDown)
+        , ((myModMask, xK_m), BoringWindows.focusMaster)
+        , ((myModMask .|. shiftMask, xK_b), BoringWindows.markBoring)
+        , ((myModMask .|. shiftMask .|. controlMask, xK_b), BoringWindows.clearBoring)
+        -- Gaps
+        , ((myModMask, xK_u), sendMessage $ ToggleGap L)
+        -- Launch gvim
+        , ((myModMask, xK_g), spawn "gvim")
+        , ((myModMask, xK_p), spawn "dmenu_run")
+        , ((myModMask .|. shiftMask, xK_p), spawn "dbus-send --dest=com.canonical.Unity2d.Dash --type=method_call /Dash com.canonical.Unity2d.Dash.activateHome")
+        -- Push window back into tiling (free up xK_t for scratchPad)
+        , ((myModMask .|. shiftMask, xK_t), withFocused $ windows.StackSet.sink)
+        -- scratchpad
+        , ((myModMask, xK_t), namedScratchpadAction scratchpads "gnome-terminal")
+        , ((myModMask, xK_a), namedScratchpadAction scratchpads "antidote")
+        -- quiting / logging out
+        , ((myModMask .|. shiftMask, xK_q), spawn "gnome-session-quit")
+        ]
+    where
+        myModMask = mod1Mask
+        tiled = ResizableTall nmaster delta ratio []
+        nmaster = 1
+        -- ratio = 0.61803399
+        ratio = 0.6
+        delta = 3/100
+
